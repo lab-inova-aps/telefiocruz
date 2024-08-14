8b357a013d1fd620d654cd3904db5ad46766ba4e
Breno Silva <brenokcc@yahoo.com.br>
2024-08-14 11:55:10 -0300

    Continue

--- backend/api/management/commands/carga_inicial.py
+++ backend/api/management/commands/carga_inicial.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.filter(username='admin').update(username='000.000.000-00')
+        call_command('loaddata', 'initial_data.json.gz')
