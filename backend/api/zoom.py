bae52f053866d0e62edcf4c139f24e01c50d7d90
Breno Silva <brenokcc@yahoo.com.br>
2024-09-05 20:07:15 -0300

    Continue

--- backend/api/zoom.py
+++ backend/api/zoom.py
@@ -9,7 +9,6 @@ ZOOM_API_SEC = os.environ.get('ZOOM_API_SEC')
 
 def view(request):
     data = cache.get(request.GET['token'])
-    data.update(role=1 if data['username'] == request.user.username else 0)
-    print(data)
+    data.update(role=request.GET['role'])
     return render(request, 'zoom.html', dict(ZOOM_SDK_KEY=ZOOM_API_KEY, ZOOM_SDK_SEC=ZOOM_API_SEC, DATA=data))
  
